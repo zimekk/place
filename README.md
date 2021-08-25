ffb811393662b862e243aae1fcf9fd639b7f148e
Marcin Makarewicz <marcin@makarewicz.eu>
2021-08-26 00:10:44 +0200

    chore: add packages

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # place
+
+[zimekk.github.io/place](https://zimekk.github.io/place/)
+
+```sh
+ssh-keygen -t rsa -b 4096 -C "" -f ACTIONS_DEPLOY_KEY -N ""
+```
---
+++ packages/web/src/containers/Home/index.tsx
@@ -0,0 +1,7 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import cx from "classnames";
+import styles from "./styles.module.scss";
+
+export default function Home() {
+  return <div className={styles.Layout}>Home</div>;
+}
