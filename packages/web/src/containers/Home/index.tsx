3a7f3422de36a9c7300ecc21b7c5ea5d07dd3bb1
Marcin Makarewicz <marcin@makarewicz.eu>
2021-08-26 00:50:39 +0200

    feat: add leaflet map

--- packages/web/src/containers/Home/index.tsx
+++ packages/web/src/containers/Home/index.tsx
@@ -1,7 +1,38 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import cx from "classnames";
 import styles from "./styles.module.scss";
 
 export default function Home() {
-  return <div className={styles.Layout}>Home</div>;
+  // https://stackoverflow.com/questions/40719689/how-to-include-leaflet-css-in-a-react-app-with-webpack
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
+      iconUrl: require("leaflet/dist/images/marker-icon.png").default,
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+    });
+  }, []);
+  // https://react-leaflet.js.org/docs/start-setup/
+  return (
+    <div className={cx(styles.Layout)}>
+      <h2>Home</h2>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        className={cx(styles.Map)}
+      >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
